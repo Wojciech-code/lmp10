0ed8081ffb520200c28b803164f092130c3fd389
zioberm <zioberm@edi.iem.pw.edu.pl>
2021-01-21 22:06:26 +0100

    Zaimplementowanie funkcji fi, obliczającą wartość dla wielomianu z bazy. Na razie bez przedziałów

--- aproksLagera.c
+++ aproksLagera.c
@@ -13,9 +13,42 @@
  * Funkcje bazowe: n - liczba funkcji a,b - granice przedzialu aproksymacji i
  * - numer funkcji x - wspolrzedna dla ktorej obliczana jest wartosc funkcji
  */
+
+//Funkcja obliczająca wartość x w funkcji podstawowej danego stopnia z bazy
 double
 fi(double a, double b, int n, int i, double x)
 {
+
+double sum=1;
+int it;
+
+double silniaI=1;
+for(it=1; it<=i; it++){
+	silniaI=silniaI*it;
+}
+
+int k;
+for(k=1; k<=i; k++){
+	double silniaK=1;
+	double jedynka=1;
+	double iks=1;
+	int it;
+	for(it=1; it<=k; it++){
+		silniaK=silniaK*it;
+		jedynka=jedynka*(-1);
+		iks=iks*x;
+	}
+
+	double silniaNK= 1;
+	int j;
+	for( j=1; j<=(i - k); j++){
+		silniaNK = silniaNK * j; 
+	}
+
+	sum+=(jedynka/silniaK) * iks * silniaI / (silniaK * silniaNK );
+}
+
+return sum;
 }
 
 /* Pierwsza pochodna fi */
